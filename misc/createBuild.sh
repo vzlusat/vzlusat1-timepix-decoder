de1c9c7687c5a3e696b1dc485c182995d777d630
klaxalk <Tomas Baca>
2017-07-16 22:01:30 +0200

    added script for creating a build archive

---
+++ misc/createBuild.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+version="1.0.3"
+
+files=(
+"src"
+"orbital_data"
+"README.md"
+"decoder.py"
+"run_as_admin.bat"
+)
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+cd $MY_PATH/..
+
+# create the directory for the build
+DIR_NAME="xray-decoder-$version"
+rm -rf $DIR_NAME
+mkdir $DIR_NAME
+
+# copy files
+for ((i=0; i < ${#files[*]}; i++));
+do
+  cp -r ${files[$i]} $DIR_NAME/.
+done
+
+# clean all .pyc files
+rm $DIR_NAME/**/**.pyc
+
+# create the tar ball
+tar -cvzf $DIR_NAME.tar.gz $DIR_NAME
+
+# remove the dirctory
+rm -rf $DIR_NAME
