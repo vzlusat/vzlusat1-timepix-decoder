d9008db55e1f0e920049e1d239d730ffda0b8cd2
Tomas Baca <tomas.baca@fel.cvut.cz>
2017-09-06 07:51:04 +0200

    new data

--- orbital_data/all/download.sh
+++ orbital_data/all/download.sh
@@ -1,8 +1,8 @@
-# get the path to this script
+# get th,,,ath to this script
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
-wget -rN --no-parent --reject "index.html*" http://147.228.97.106/download/
+wget -rN --no-parent --reject 'index.html*|*.wav' http://147.228.97.106/download/
 
 git rm ./**/*.wav
 git rm ./147.228.97.106/download/old*
