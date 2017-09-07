fe198f779b699f9ac79297471bc7f5fe5918d6e5
Tomas Baca <tomas.baca@fel.cvut.cz>
2017-09-07 12:02:52 +0200

    updated comments and processing sripts

--- processing/adrenalin1.py
+++ processing/adrenalin1.py
@@ -23,6 +23,10 @@ images.append(loadImage(402, 1, path))
 images.append(loadImage(404, 1, path))
 images.append(loadImage(807, 1, path))
 images.append(loadImage(808, 1, path))
+# images.append(loadImage(809, 1, path)) # full of electrons
+images.append(loadImage(810, 1, path))
+images.append(loadImage(811, 1, path))
+images.append(loadImage(812, 1, path))
 
 for i in range(len(images)):
 
@@ -46,25 +50,27 @@ fig = plt.figure(1)
 
 ax = fig.add_subplot(1, 2, 1)
 im = ax.imshow(merged, interpolation='none', cmap='nipy_spectral_r')
-ax.set_xlabel("Column [-]")
-ax.set_ylabel("Row [-]")
-ax.set_title("Stacket images, no filtering")
+ax.set_xlabel("Column [-]", fontsize=25)
+ax.set_ylabel("Row [-]", fontsize=25)
+ax.set_title("Stacket images, no filtering", fontsize=25)
 
 divider = make_axes_locatable(fig.gca())
 cax = divider.append_axes("right", size="5%", pad=0.2)
 cbar = fig.colorbar(im, cax)
-cbar.ax.set_ylabel('[keV]', rotation=270)
+cbar.ax.set_ylabel('[keV]', rotation=270, fontsize=12)
 
 ax = fig.add_subplot(1, 2, 2)
 im = ax.imshow(merged_filtered, interpolation='none', cmap='nipy_spectral_r')
-ax.set_xlabel("Column [-]")
-ax.set_ylabel("Row [-]")
-ax.set_title("Stacket images, individually filtered")
+ax.set_xlabel("Column [-]", fontsize=25)
+ax.set_ylabel("Row [-]", fontsize=25)
+ax.set_title("Stacket images, individually filtered", fontsize=25)
 
 divider = make_axes_locatable(fig.gca())
 cax = divider.append_axes("right", size="5%", pad=0.2)
 cbar = fig.colorbar(im, cax)
-cbar.ax.set_ylabel('[keV]', rotation=270)
+cbar.ax.set_ylabel('[keV]', rotation=270, fontsize=12)
+
+plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.15, hspace=0.05)
 
 # create sum
 
@@ -88,5 +94,11 @@ x = numpy.linspace(1, 256, 256)
 ax.plot(x, sums)
 
 ax.axis([1, 256, numpy.min(sums), numpy.max(sums)])
+
+ax.set_xlabel("Column [-]", fontsize=25)
+ax.set_ylabel("Active pixels [-]", fontsize=25)
+ax.set_title("Column sum over stacked images", fontsize=25)
+
+plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.15, hspace=0.05)
 
 plt.show()
