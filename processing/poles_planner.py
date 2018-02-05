bddcbef2bf67d34b50afcab86714f676eac2a663
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-02-05 17:48:47 +0100

    updated poles planner

--- processing/poles_planner.py
+++ processing/poles_planner.py
@@ -8,25 +8,30 @@ import time
 
 from include.baseMethods import *
 
-from_time = "04.02.2018 20:00:00"
-to_time = "06.02.2018 20:00:00"
+from_time = "06.02.2018 20:00:00"
+to_time = "07.02.2018 20:00:00"
 
-anomaly_lat = -30.0
-anomaly_long = -40.0
-anomaly_size = 45.0
 desired_fill = 500
 max_exposure = 1
 hkc_buffer_time = 300
 
-aprox_pole = 30
-latitude_limit = 8
+aprox_pole = 20
+latitude_limit = 10
 
-from_idx = 14425
-to_idx = 15115
+from_idx = 5804
+to_idx = 6815
 outliers=[]
+
+anomaly_lat = -30.0
+anomaly_long = -40.0
+anomaly_size = 40.0
 
 pcolor_min = 0
 pcolor_max = 7
+
+mesh_size = 200
+
+step_size = 10
 
 epsilon=1.0
 x_label = 'Pixel count'
@@ -50,7 +55,7 @@ doses_log_wrapped, lats_wrapped, lons_wrapped = wrapAround(doses_log, lats_orig,
 #{ RBF interpolation
 
 # create meshgrid for RBF
-x_meshgrid, y_meshgrid = createMeshGrid(360)
+x_meshgrid, y_meshgrid = createMeshGrid(mesh_size)
 
 # calculate RBF from log data
 rbf_lin = Rbf(lats_wrapped, lons_wrapped, doses_wrapped, function='multiquadric', epsilon=epsilon, smooth=1)
@@ -74,140 +79,192 @@ file_name = directory+"/{}_anomaly.pln".format(from_time).replace(' ', '_').repl
 
 from scipy import spatial
 
-points_x = []
-points_y = []
-
-for j in range(0, 1000):
-    points_x.append(0)
-    points_y.append(0)
-
-n_points = 0
+lats = []
+lons = []
+times = []
+orbits = []
+orbit_line_lats = []
+orbit_line_lons = []
 
 def is_free(x, y):
 
-    for j in range(0, n_points):
+    for j in range(0, len(lats)):
 
-        if math.sqrt(math.pow(x-points_x[j], 2)+math.pow(y-points_y[j], 2)) <= 30:
+        # if dist(x, y, lats[j], lons[j]) < 19:
 
-            return False
+        #     return False
+
+        if x < 0 and lats[j] < 0:
+            if abs(y - lons[j]) < 18:
+
+                return False
+
+        if x > 0 and lats[j] > 0:
+            if abs(y - lons[j]) < 18:
+
+                return False
 
     return True
+
+def add(x, y, best_time, orbit_n):
+
+    if is_free(x, y):
+
+        times.append(best_time)
+        lats.append(x)
+        lons.append(y)
+        orbits.append(orbit_n)
+
+        return True
+
+    else:
+
+        return False
 
 with open(file_name, "w") as file:
 
     print("t: {}".format(t))
 
-    lats = []
-    lons = []
-    times = []
-
     # find the closest point to the anomaly in each orbit
-    i = t
-    best_time = 0
+    for g in range(0, 1):
 
-    max_pxl_count = 0
-    max_time = 0
-    max_lat = 0
-    max_lon = 0
-    are_in = False
+        i = t
+        best_time = 0
+        best_would_be_pxl_count = 0
+        max_pxl_count = 0
+        max_time = 0
+        max_lat = 0
+        max_lon = 0
+        are_in = False
 
-    in_first = False
-    in_second = False 
+        in_first = False
+        in_second = False 
 
-    while i <= t_end:
+        orbit_counter = 0
 
-        latitude, longitude, tle_date = getLatLong(int(i))
+        while i <= t_end:
 
-        if not are_in:
+            latitude, longitude, tle_date = getLatLong(int(i))
 
-            fut_latitude, fut_longitude, fut_tle_date = getLatLong(i+10)
+            fut_latitude, fut_longitude, fut_tle_date = getLatLong(i+step_size)
 
-            # we are not in the equator belt
-            if abs(latitude) > 40:
+            if (g == 0):
+                if i > t: 
+                    if (dist(orbit_line_lats[-1], orbit_line_lons[-1], latitude, longitude) > 2.0):
+                        orbit_line_lats.append(latitude)
+                        orbit_line_lons.append(longitude)
+                else:
+                    orbit_line_lats.append(latitude)
+                    orbit_line_lons.append(longitude)
 
-                # we enter from the equator belt
-                if (((latitude > -45) and (fut_latitude < -45)) or ((latitude < 45) and (fut_latitude > 45))):
+            if latitude < 0 and fut_latitude > 0:
+                orbit_counter += 1
 
-                    are_in = True
-                    in_first = True
-                    max_pxl_count = 0
-                    best_time = 0
-                    print("Entered the first: latitude: {}, longitude: {}".format(latitude, longitude))
+            if not are_in:
 
-                elif ((latitude > (90 - latitude_limit)) or (latitude < (-90 + latitude_limit))):
+                # we are not in the equator belt
+                if abs(latitude) > (90 - (2*aprox_pole + 1)):
 
-                    are_in = True
-                    in_second = True
-                    in_first = False
-                    max_pxl_count = 0
-                    best_time = 0
-                    print("Entered the second: latitude: {}, longitude: {}".format(latitude, longitude))
+                    # we enter from the equator belt
+                    if (((latitude > (-90 + 2*aprox_pole)) and (fut_latitude < (-90 + 2*aprox_pole))) or ((latitude < (90 - 2*aprox_pole)) and (fut_latitude > (90 - 2*aprox_pole)))):
 
-        if are_in:
+                        are_in = True
+                        in_first = True
+                        in_second = False
+                        max_pxl_count = 0
+                        best_would_be_pxl_count = 0
+                        best_time = 0
+                        print("Entered the first: latitude: {}, longitude: {}".format(latitude, longitude))
 
-            pxl_count = doses_rbf_lin[int(math.floor(360*((longitude+180)/360))), int(math.floor(360*((latitude+90)/180)))]
+                    elif ((latitude > (90 - latitude_limit)) or (latitude < (-90 + latitude_limit))):
 
-            # we left the area
-            # if (((latitude > 0) and (latitude > (90 - latitude_limit))) or ((latitude < 0) and (latitude < (-90 + latitude_limit)))):
+                        are_in = True
+                        in_second = True
+                        in_first = False
+                        max_pxl_count = 0
+                        best_would_be_pxl_count = 0
+                        best_time = 0
+                        print("Entered the second: latitude: {}, longitude: {}".format(latitude, longitude))
 
-            if in_first:
+            if are_in:
 
                 # times.append(i)
+                # orbits.append(orbit_n)
 
-                if (((latitude > 0) and (latitude > (90 - latitude_limit))) or ((latitude < 0) and (latitude < (-90 + latitude_limit)))):
+                pxl_count = doses_rbf_log[int(math.floor(mesh_size*((longitude+180)/360))), int(math.floor(mesh_size*((latitude+90)/180)))]
 
-                    are_in = False
-                    in_first = False
+                # we left the area
+                # if (((latitude > 0) and (latitude > (90 - latitude_limit))) or ((latitude < 0) and (latitude < (-90 + latitude_limit)))):
 
-                    if best_time > 1 and is_free(latitude, longitude):
+                if in_first:
 
-                        times.append(best_time)
+                    if (((latitude > 0) and (latitude > (90 - latitude_limit))) or ((latitude < 0) and (latitude < (-90 + latitude_limit)))):
 
-                        points_x[n_points] = latitude
-                        points_y[n_points] = longitude
+                        are_in = False
+                        in_first = False
 
-                        print("Appending: latitude: {}, longitude: {}".format(latitude, longitude))
+                        if best_time > 1:
 
-                        n_points += 1
-                    
-                    max_pxl_count = 0
-                    best_time = 0
+                            if add(max_lat, max_lon, best_time, orbit_counter):
 
-                    print("Left first: latitude: {}, longitude: {}".format(latitude, longitude))
+                                print("Appending: latitude: {}, longitude: {}".format(max_lat, max_lon))
+                        
+                            else:
 
-            elif in_second:
+                                print("Could not add the point, too close.")
 
-                if (abs(latitude - (90 - 2*aprox_pole)) < 1) or (abs(latitude - (-90 + 2*aprox_pole)) < 1):
+                            max_pxl_count = 0
+                            best_would_be_pxl_count = 0
+                            best_time = 0
 
-                    are_in = False
-                    in_second = False
+                        print("Left first: latitude: {}, longitude: {}".format(latitude, longitude))
 
-                    if best_time > 1 and is_free(latitude, longitude):
+                elif in_second:
 
-                        times.append(best_time)
+                    if (abs(latitude - (90 - 2*aprox_pole)) < 1) or (abs(latitude - (-90 + 2*aprox_pole)) < 1):
 
-                        points_x[n_points] = latitude
-                        points_y[n_points] = longitude
+                        are_in = False
+                        in_second = False
 
-                        n_points += 1
+                        if best_time > 1:
 
-                    max_pxl_count = 0
-                    best_time = 0
+                            if add(max_lat, max_lon, best_time, orbit_counter):
 
-                    print("Left second: latitude: {}, longitude: {}".format(latitude, longitude))
+                                print("Appending: latitude: {}, longitude: {}".format(max_lat, max_lon))
 
-            if pxl_count >= max_pxl_count:
+                            else:
 
-                if is_free(latitude, longitude):
+                                print("Could not add the point, too close.")
 
-                    max_pxl_count = pxl_count
-                    best_time = i
-                    print("Found max: latitude: {}, longitude: {}, value: {}".format(latitude, longitude, pxl_count))
+                            max_pxl_count = 0
+                            best_would_be_pxl_count = 0
+                            best_time = 0
 
-        i += 10
+                        print("Left second: latitude: {}, longitude: {}".format(latitude, longitude))
+
+                if pxl_count > best_would_be_pxl_count:
+
+                    best_would_be_pxl_count = pxl_count
+
+                if pxl_count > max_pxl_count:
+
+                    if is_free(latitude, longitude):
+
+                        if (pxl_count+1.5) >= best_would_be_pxl_count:
+
+                            if dist(latitude, longitude, anomaly_lat, anomaly_long) > anomaly_size:
+
+                                max_pxl_count = pxl_count
+                                best_time = i
+                                max_lat = latitude
+                                max_lon = longitude
+                                print("Found max: latitude: {}, longitude: {}, value: {}".format(latitude, longitude, pxl_count))
+
+            i += step_size
 
 # scan the surroundigs of the locations
     out_lats = []
+    out_orbits = []
     out_lons = []
     out_times = []
     out_pxl_counts = []
@@ -224,18 +281,19 @@ with open(file_name, "w") as file:
             time = datetime.datetime.utcfromtimestamp(t_now-59-hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
             file.write(time+"\tP\tsleep 4000\r\n")
             time = datetime.datetime.utcfromtimestamp(t_now-55-hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
-            file.write(time+"\tP\tx sp 404 1 70 0 1 {} 80 0 0 1\r\n".format(1+2+4+8+32))
-            time = datetime.datetime.utcfromtimestamp(t_now-50-hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
-            file.write(time+"\tP\th conf 3 3 0 01400000000000000000\r\n")
+            file.write(time+"\tP\tx sp 400 1 70 0 1 {} 80 0 0 1\r\n".format(1+2+32))
+            # time = datetime.datetime.utcfromtimestamp(t_now-50-hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
+            # file.write(time+"\tP\th conf 3 3 0 01400000000000000000\r\n")
             
-        time = datetime.datetime.utcfromtimestamp(t_now-hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
-        file.write(time+"\tP\th go 3 1\r\n")
+        # time = datetime.datetime.utcfromtimestamp(t_now-hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
+        # file.write(time+"\tP\th go 3 1\r\n")
         
         latitude, longitude, tle_date = getLatLong(int(t_now))
         out_lats.append(latitude)
+        out_orbits.append(orbits[i])
         out_lons.append(longitude)
         out_times.append(t_now)
-        pxl_count = doses_rbf_lin[int(math.floor(100*(latitude+90)/180)), int(math.floor(100*(longitude+180)/360))]
+        pxl_count = doses_rbf_lin[int(math.floor(mesh_size*(latitude+90)/180)), int(math.floor(mesh_size*(longitude+180)/360))]
         if pxl_count < 0:
             pxl_count = 0
         out_pxl_counts.append(pxl_count)
@@ -248,24 +306,24 @@ with open(file_name, "w") as file:
             if desired_exposure == 0:
                 desired_exposure = 1
 
-        total_chunks += 1+4+16+1+(round(((desired_exposure/1000.0)*pxl_count)/20))+1
+        total_chunks += 16+1+(round(((desired_exposure/1000.0)*pxl_count)/20))+1
 
-        # if desired_exposure != last_exposure:
-        #   time = datetime.datetime.utcfromtimestamp(t_now-20).strftime('%Y-%m-%d %H:%M:%S')
-        #   file.write(time+"\t\tx se {}\r\n".format(desired_exposure))
-        #   last_exposure = desired_exposure
+        if desired_exposure != last_exposure:
+          time = datetime.datetime.utcfromtimestamp(t_now-20).strftime('%Y-%m-%d %H:%M:%S')
+          file.write(time+"\t\tx se {}\r\n".format(desired_exposure))
+          last_exposure = desired_exposure
 
-        # time = datetime.datetime.utcfromtimestamp(t_now-15).strftime('%Y-%m-%d %H:%M:%S')
-        # file.write(time+"\t\tx m\r\n")
+        time = datetime.datetime.utcfromtimestamp(t_now-15).strftime('%Y-%m-%d %H:%M:%S')
+        file.write(time+"\t\tx m\r\n")
 
         print("latitude: {}, longitude: {}, intensity: {}".format(latitude, longitude, pxl_count))
 
         # time = datetime.datetime.utcfromtimestamp(t_now+hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
         # file.write(time+"\tP\th stop 3 1\r\n")
 
-    # time = datetime.datetime.utcfromtimestamp(out_times[-1]+300).strftime('%Y-%m-%d %H:%M:%S')
-    # file.write(time+"\tP\tx pwr 0\r\n")
-    # time = datetime.datetime.utcfromtimestamp(out_times[-1]+300+hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
+    time = datetime.datetime.utcfromtimestamp(out_times[-1]+300).strftime('%Y-%m-%d %H:%M:%S')
+    file.write(time+"\tP\tx pwr 0\r\n")
+    time = datetime.datetime.utcfromtimestamp(out_times[-1]+300+hkc_buffer_time).strftime('%Y-%m-%d %H:%M:%S')
     
 fig1 = []
 
@@ -273,26 +331,99 @@ def plot_everything(*args):
 
     fig1 = plt.figure(1)
 
-    ax1 = plt.subplot2grid((1, 1), (0, 0))
+    ax1 = plt.subplot2grid((3, 2), (0, 0), colspan=2, rowspan=2)
+    # ax1 = plt.subplot2grid((1, 1), (0, 0))
 
+    #{ map
+    
     m = createMap('cyl')
-
+    
     x_m_meshgrid, y_m_meshgrid = m(y_meshgrid, x_meshgrid)
-
+    
     m.pcolor(x_m_meshgrid, y_m_meshgrid, doses_rbf_log, cmap=my_cm, vmin=pcolor_min, vmax=pcolor_max)
-
+    
     cb = m.colorbar(location="bottom", label="Z") # draw colorbar
-
+    
+    for i in range(len(orbit_line_lats)):
+        x, y = m(orbit_line_lons[i], orbit_line_lats[i])
+        m.scatter(x, y, 20, marker='o', color='grey', zorder=5)
+    
     for i in range(len(out_lats)):
         x, y = m(out_lons[i], out_lats[i])
         m.scatter(x, y, 80, marker='o', color='k', zorder=10)
-
+    
+        plt.text(x+1, y+1, '{}'.format(out_orbits[i]), fontsize=13, fontweight='bold', ha='left', va='bottom', color='k', zorder=10)
+    
     # cb.set_label('log10('+x_label+') '+x_units)
     plt.title('Anomaly scanning, starting at {}'.format(from_time))
-
+    
     plt.subplots_adjust(left=0.025, bottom=0.05, right=0.975, top=0.95, wspace=0.1, hspace=0.1)
-
+    
     plt.savefig(directory+"/{}_anomaly.jpg".format(from_time).replace(' ', '_').replace(':', '_'), dpi=60, bbox_inches='tight')
+    
+    #} end of globe south
+
+    ax1 = plt.subplot2grid((3, 2), (2, 0))
+
+    #{ globe south
+    
+    m = createMap('ortho', -90, 0)
+    
+    x_m_meshgrid, y_m_meshgrid = m(y_meshgrid, x_meshgrid)
+    
+    m.pcolor(x_m_meshgrid, y_m_meshgrid, doses_rbf_log, cmap=my_cm, vmin=pcolor_min, vmax=pcolor_max)
+    
+    cb = m.colorbar(location="bottom", label="Z") # draw colorbar
+    
+    # for i in range(len(orbit_line_lats)):
+    #     x, y = m(orbit_line_lons[i], orbit_line_lats[i])
+    #     m.scatter(x, y, 20, marker='o', color='grey', zorder=5)
+    
+    for i in range(len(out_lats)):
+        x, y = m(out_lons[i], out_lats[i])
+        m.scatter(x, y, 80, marker='o', color='k', zorder=10)
+    
+        # plt.text(x+1, y+1, '{}'.format(out_orbits[i]), fontsize=13, fontweight='bold', ha='left', va='bottom', color='k', zorder=10)
+    
+    # cb.set_label('log10('+x_label+') '+x_units)
+    # plt.title('Anomaly scanning, starting at {}'.format(from_time))
+    
+    plt.subplots_adjust(left=0.025, bottom=0.05, right=0.975, top=0.95, wspace=0.1, hspace=0.1)
+    
+    # plt.savefig(directory+"/{}_anomaly.jpg".format(from_time).replace(' ', '_').replace(':', '_'), dpi=60, bbox_inches='tight')
+    
+    #} end of globe south
+
+    ax1 = plt.subplot2grid((3, 2), (2, 1))
+
+    #{ globe south
+    
+    m = createMap('ortho', +90, 0)
+    
+    x_m_meshgrid, y_m_meshgrid = m(y_meshgrid, x_meshgrid)
+    
+    m.pcolor(x_m_meshgrid, y_m_meshgrid, doses_rbf_log, cmap=my_cm, vmin=pcolor_min, vmax=pcolor_max)
+    
+    cb = m.colorbar(location="bottom", label="Z") # draw colorbar
+    
+    # for i in range(len(orbit_line_lats)):
+    #     x, y = m(orbit_line_lons[i], orbit_line_lats[i])
+    #     m.scatter(x, y, 20, marker='o', color='grey', zorder=5)
+    
+    for i in range(len(out_lats)):
+        x, y = m(out_lons[i], out_lats[i])
+        m.scatter(x, y, 80, marker='o', color='k', zorder=10)
+    
+        # plt.text(x+1, y+1, '{}'.format(out_orbits[i]), fontsize=13, fontweight='bold', ha='left', va='bottom', color='k', zorder=10)
+    
+    # cb.set_label('log10('+x_label+') '+x_units)
+    # plt.title('Anomaly scanning, starting at {}'.format(from_time))
+    
+    plt.subplots_adjust(left=0.025, bottom=0.05, right=0.975, top=0.95, wspace=0.1, hspace=0.1)
+    
+    # plt.savefig(directory+"/{}_anomaly.jpg".format(from_time).replace(' ', '_').replace(':', '_'), dpi=60, bbox_inches='tight')
+    
+    #} end of globe south
 
     plt.show()
 
@@ -307,8 +438,6 @@ with open(file_name, "w") as file:
     file.write("desired fill: {} px per image\r\n".format(desired_fill))
     file.write("estimated number of chunks: {}\r\n".format(int(total_chunks)))
     file.write("based on data range: {} to {}\r\n".format(from_idx, to_idx))
-    file.write("anomaly lat, long: {}, {}\r\n".format(anomaly_lat, anomaly_long))
-    file.write("anomaly radius: {} deg\r\n".format(anomaly_size))
     file.write("max blunt exposure time: {} ms".format(max_exposure))
 
 pid = os.fork()
