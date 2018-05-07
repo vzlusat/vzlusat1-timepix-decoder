7927496236b554fd351ec499f161bac8fdcf66e5
Tomas Baca <tomas.baca@fel.cvut.cz>
2018-05-07 11:09:34 +0200

    new orbital data, updated tle

--- processing/combined_planner.py
+++ processing/combined_planner.py
@@ -9,8 +9,8 @@ import numpy as np
 
 from include.baseMethods import *
 
-from_time = "03.04.2018 20:00:00"
-to_time = "04.04.2018 20:00:00"
+from_time = "04.05.2018 20:00:00"
+to_time = "05.05.2018 20:00:00"
 
 desired_fill = 50
 max_exposure = 0.03
