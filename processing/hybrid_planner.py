aa69dbbb411ac582802cbb571184f057dfd5b5fa
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-09-23 17:58:56 +0200

    new orbital data, updated tle, update hybrid planner

--- processing/hybrid_planner.py
+++ processing/hybrid_planner.py
@@ -10,7 +10,7 @@ import numpy as np
 from include.baseMethods import *
 
 from_time = "12.07.2019 10:00:00"
-to_time = "12.07.2019 22:00:00"
+to_time = "13.07.2019 10:00:00"
 
 desired_fill = 50
 ax_exposure = 0.03
@@ -172,15 +172,15 @@ def plot_everything(*args):
     ax1 = plt.subplot2grid((1, 2), (0, 0), colspan=1, rowspan=2)
 
     # #{ map
-    
+
     m = createMap('cyl')
-    
+
     x_m_meshgrid, y_m_meshgrid = m(y_meshgrid, x_meshgrid)
-    
+
     m.pcolor(x_m_meshgrid, y_m_meshgrid, doses_rbf_log, cmap=my_cm, vmin=pcolor_min, vmax=pcolor_max)
-    
+
     cb = m.colorbar(location="bottom", label="Log of relative intensity [px flux]") # draw colorbar
-    
+
     for i in range(len(lats)):
         x, y = m(lons[i], lats[i])
         if states[i]:
@@ -188,26 +188,26 @@ def plot_everything(*args):
         else:
             color = 'blue'
         m.scatter(x, y, 20, marker='o', color=color, zorder=5)
-    
+
         # plt.text(x+1, y+1, '{}'.format(out_orbits[i]), fontsize=13, fontweight='bold', ha='left', va='bottom', color='k', zorder=10)
-    
+
     # cb.set_label('log10('+x_label+') '+x_units)
     plt.title('Combined scanning, starting on {}'.format(from_time))
-    
+
     plt.subplots_adjust(left=0.025, bottom=0.05, right=0.975, top=0.95, wspace=0.1, hspace=0.1)
-    
+
     plt.savefig(directory+"/{}_combined.png".format(from_time).replace(' ', '_').replace(':', '_'), dpi=60, bbox_inches='tight')
-    
+
     # #} end of globe south
 
     ax1 = plt.subplot2grid((1, 2), (0, 1), colspan=1, rowspan=2)
 
     # #{ pxlcount
-    
+
     plt.plot(times, pxl_counts)
-    
+
     plt.subplots_adjust(left=0.025, bottom=0.05, right=0.975, top=0.95, wspace=0.1, hspace=0.1)
-    
+
     # #} end of globe south
 
     plt.show()
