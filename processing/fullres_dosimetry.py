6f598ef8a54b74e0567d69bba04fa0f4d0633467
klaxalk <klaxalk@gmail.com>
2020-10-07 17:55:52 +0200

    updated plotting scripts

--- processing/fullres_dosimetry.py
+++ processing/fullres_dosimetry.py
@@ -8,19 +8,20 @@ import matplotlib.ticker as ticker # for colorbar
 from include.baseMethods import *
 
 from_to = numpy.array([
-# [36352, 36671], # 1st full res
-# [36672, 37034], # 2nd full res
-# [37103, 37862], # 3rd full res
-# [37863, 38587], # 4th full res
-# [38604, 39191], # 5th full res
-# [39194, 39961], # 6th full res
-# [39962, 40568], # 7th full res
-# [40600, 41429], # 8th full res
-# [41446, 42354], # 9th full res
-# [42355, 43038], # 10th full res
-# [43072, 43963], # 11th full res
-# [43964, 44569], # 12th full res
-# [44648, 45813], # 13th full res
+[36352, 36671], # 1st full res
+[36672, 37034], # 2nd full res
+[37103, 37862], # 3rd full res
+[37863, 38587], # 4th full res
+[38604, 39191], # 5th full res
+[39194, 39961], # 6th full res
+[39962, 40568], # 7th full res
+[40600, 41429], # 8th full res
+[41446, 42354], # 9th full res
+[42355, 43038], # 10th full res
+[43072, 43963], # 11th full res
+[43964, 44569], # 12th full res
+[44648, 45813], # 13th full res
+[45826, 46193], # 14th full res
 ])
 outliers=[]
 
